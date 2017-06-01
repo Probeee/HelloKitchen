dc0cc0c1b5dc7082756005d8a41428f2d34e2552
Probeee <r941182@gmail.com>
2017-06-01 18:28:27 +0800

    20170601-Location

--- app/src/main/java/tw/org/iii/hellokitchen/Frag_Foods_ShopSearch.java
+++ app/src/main/java/tw/org/iii/hellokitchen/Frag_Foods_ShopSearch.java
@@ -1,9 +1,11 @@
 package tw.org.iii.hellokitchen;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,25 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -34,8 +52,9 @@ public class Frag_Foods_ShopSearch extends Fragment implements OnMapReadyCallbac
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    double latitude ;
-    double longitude;
+    double latitude =0;
+    double longitude = 0;
+
 
 
     public Frag_Foods_ShopSearch()
@@ -83,6 +102,9 @@ public class Frag_Foods_ShopSearch extends Fragment implements OnMapReadyCallbac
         // Inflate the layout for this fragment
         tv_location = (TextView) v.findViewById(R.id.textView_shopSearch);
         InitialLocation();
+
+
+
         return v;
     }
 
@@ -97,6 +119,9 @@ public class Frag_Foods_ShopSearch extends Fragment implements OnMapReadyCallbac
             mMapView.onResume();
             mMapView.getMapAsync(this);
         }
+        StringBuilder sbValue = new StringBuilder(sbMethod());
+        PlacesTask placesTask = new PlacesTask();
+        placesTask.execute(sbValue.toString());
     }
 
     private void InitialLocation()
@@ -136,4 +161,238 @@ public class Frag_Foods_ShopSearch extends Fragment implements OnMapReadyCallbac
         CameraPosition position = CameraPosition.builder().target(new LatLng(latitude,longitude)).zoom(16).bearing(0).tilt(0).build();
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(position));
     }
+
+    public StringBuilder sbMethod()
+    {
+
+        //use your current location here
+
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        sb.append("location=" + latitude + "," + longitude);
+        sb.append("&radius=1000");
+        sb.append("&language=zh-TW");
+        sb.append("&keyword=supermarket+%E8%B6%85%E5%B8%82+%e8%8f%9c%e5%b8%82%e5%a0%b4%0d%0a");
+        sb.append("&sensor=true");
+        sb.append("&key=AIzaSyDdKE-JYbnGeOJCZ6CeL7nt4cjvZdqqXDU");
+
+        Log.d("Map", "api: " + sb.toString());
+
+        return sb;
+    }
+    private class PlacesTask extends AsyncTask<String, Integer, String>
+    {
+
+        String data = null;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected String doInBackground(String... url)
+        {
+            try
+            {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(String result)
+        {
+            ParserTask parserTask = new ParserTask();
+
+            // Start parsing the Google places in JSON format
+            // Invokes the "doInBackground()" method of the class ParserTask
+            parserTask.execute(result);
+        }
+    }
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e)
+        {
+            Log.d("Exception download url", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
+
+        JSONObject jObject;
+
+        // Invoked by execute() method of this object
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... jsonData) {
+
+            List<HashMap<String, String>> places = null;
+            Place_JSON placeJson = new Place_JSON();
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+
+                places = placeJson.parse(jObject);
+
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
+            }
+            return places;
+        }
+
+        // Executed after the complete execution of doInBackground() method
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> list)
+        {
+
+            Log.d("Map", "list size: " + list.size());
+            // Clears all the existing markers;
+            //mgoogleMap.clear();
+
+            for (int i = 0; i < list.size(); i++) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Getting a place from the places list
+                HashMap<String, String> hmPlace = list.get(i);
+
+
+                // Getting latitude of the place
+                double lat = Double.parseDouble(hmPlace.get("lat"));
+
+                // Getting longitude of the place
+                double lng = Double.parseDouble(hmPlace.get("lng"));
+
+                // Getting name
+                String name = hmPlace.get("place_name");
+
+                Log.d("Map", "place: " + name);
+
+                // Getting vicinity
+                String vicinity = hmPlace.get("vicinity");
+
+                LatLng latLng = new LatLng(lat, lng);
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                markerOptions.title(name + " : " + vicinity);
+
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+
+                // Placing a marker on the touched position
+                Marker m = mgoogleMap.addMarker(markerOptions);
+
+            }
+        }
+    }
+    public class Place_JSON {
+
+        /**
+         * Receives a JSONObject and returns a list
+         */
+        public List<HashMap<String, String>> parse(JSONObject jObject) {
+
+            JSONArray jPlaces = null;
+            try {
+                /** Retrieves all the elements in the 'places' array */
+                jPlaces = jObject.getJSONArray("results");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            /** Invoking getPlaces with the array of json object
+             * where each json object represent a place
+             */
+            return getPlaces(jPlaces);
+        }
+
+        private List<HashMap<String, String>> getPlaces(JSONArray jPlaces) {
+            int placesCount = jPlaces.length();
+            List<HashMap<String, String>> placesList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> place = null;
+
+            /** Taking each place, parses and adds to list object */
+            for (int i = 0; i < placesCount; i++) {
+                try {
+                    /** Call getPlace with place JSON object to parse the place */
+                    place = getPlace((JSONObject) jPlaces.get(i));
+                    placesList.add(place);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            return placesList;
+        }
+
+        /**
+         * Parsing the Place JSON object
+         */
+        private HashMap<String, String> getPlace(JSONObject jPlace) {
+
+            HashMap<String, String> place = new HashMap<String, String>();
+            String placeName = "-NA-";
+            String vicinity = "-NA-";
+            String latitude = "";
+            String longitude = "";
+            String reference = "";
+
+            try {
+                // Extracting Place name, if available
+                if (!jPlace.isNull("name")) {
+                    placeName = jPlace.getString("name");
+                }
+
+                // Extracting Place Vicinity, if available
+                if (!jPlace.isNull("vicinity")) {
+                    vicinity = jPlace.getString("vicinity");
+                }
+
+                latitude = jPlace.getJSONObject("geometry").getJSONObject("location").getString("lat");
+                longitude = jPlace.getJSONObject("geometry").getJSONObject("location").getString("lng");
+                reference = jPlace.getString("reference");
+
+                place.put("place_name", placeName);
+                place.put("vicinity", vicinity);
+                place.put("lat", latitude);
+                place.put("lng", longitude);
+                place.put("reference", reference);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return place;
+        }
+    }
+
+
 }
