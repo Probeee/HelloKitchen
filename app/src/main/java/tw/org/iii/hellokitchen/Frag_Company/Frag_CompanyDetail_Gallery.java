76cff41a65f7929231b85fd47379d91ef2edc1dd
Probeee <r941182@gmail.com>
2017-06-18 03:42:31 +0800

    Simple Company Info without gallery

--- app/src/main/java/tw/org/iii/hellokitchen/Frag_Company/Frag_CompanyDetail_Gallery.java
+++ app/src/main/java/tw/org/iii/hellokitchen/Frag_Company/Frag_CompanyDetail_Gallery.java
@@ -1,13 +1,26 @@
 package tw.org.iii.hellokitchen.Frag_Company;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import tw.org.iii.hellokitchen.R;
+import tw.org.iii.hellokitchen.Utility.CustomOkHttp3Downloader;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +37,10 @@ public class Frag_CompanyDetail_Gallery extends Fragment {
     private String mParam1;
     private String mParam2;
 
-
+    String companyId;
+    String companyName;
+    String companyLogo;
+    String companyCover;
     public Frag_CompanyDetail_Gallery() {
         // Required empty public constructor
     }
@@ -57,10 +73,79 @@ public class Frag_CompanyDetail_Gallery extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag__company_detail__gallery, container, false);
+        View v = inflater.inflate(R.layout.frag__company_detail__gallery, container, false);
+        GetInfo();
+        textView_companyName = (TextView)v.findViewById(R.id.textView_Company_Name_detail);
+        textView_companyName.setText(companyName);
+        linearCover = (LinearLayout)v.findViewById(R.id.linearLayout_company_cover);
+        LoadCover(linearCover);
+        imageView = (ImageView)v.findViewById(R.id.imageView_CompanyLogo);
+        LoadLogo(imageView);
+        return v;
     }
 
+
+
+    private void GetInfo()
+    {
+        companyId = getArguments().getString("companyId");
+        companyName = getArguments().getString("companyName");
+        companyLogo = getArguments().getString("company_logo");
+        companyCover = getArguments().getString("company_cover");
+    }
+
+    private void LoadCover(final LinearLayout ll)
+    {
+        try
+        {
+            //用Picasso載入標題圖片
+            Picasso picasso = new Picasso.Builder(getActivity())
+                    .downloader(new CustomOkHttp3Downloader(getActivity()))
+                    .build();
+            Picasso.setSingletonInstance(picasso);
+        }
+        catch (IllegalStateException ignored)
+        {
+
+        }
+
+        Picasso.with(getActivity()).load(companyCover).config(Bitmap.Config.ALPHA_8).into(new Target()
+        {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from)
+            {
+                ll.setBackground(new BitmapDrawable(getApplicationContext().getResources(), bitmap));
+                ll.getBackground().setAlpha(80);//設定非透明度60
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable)
+            {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable)
+            {
+
+            }
+        });
+    }
+    private void LoadLogo(ImageView im)
+    {
+        Picasso
+                .with(getActivity())
+                .load(companyLogo)
+                .config(Bitmap.Config.ALPHA_8)
+                .placeholder(R.drawable.photo)   // optional
+                .error(R.drawable.icon_pictureloading_error)      // optional
+                .into(im);
+    }
+    ImageView imageView;
+    TextView textView_companyName ;
+    LinearLayout linearCover;
+    GridView gridView;
 }
--- app/src/main/java/tw/org/iii/hellokitchen/Utility/TheDefined.java
+++ app/src/main/java/tw/org/iii/hellokitchen/Utility/TheDefined.java
@@ -44,8 +44,8 @@ public class TheDefined
     }
 
     /*server URL*/
-    public static final String Web_Server_URL = "http://192.168.1.110:8080/HelloKitchen0613";
-  // public static final String Web_Server_URL = "http://192.168.0.103:8080/HelloKitchen0611";
+    //public static final String Web_Server_URL = "http://192.168.1.110:8080/HelloKitchen0613";
+    public static final String Web_Server_URL = "http://192.168.0.101:8080/HelloKitchen0613";
     /*JSON Key & Value*/
    public static final String Android_JSON_Key_Member_Id = "Android_JSON_Key_Member_Id";
     public static final String Android_JSON_Key_Member_Name = "Android_JSON_Key_Member_Name";
